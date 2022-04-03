b7fe93fd86ddf6b8deda2587555244aca3540ae6
César Okada <cesar.okada@fatec.sp.gov.br>
2022-04-03 17:16:48 -0300

    Commit dos exercicios da lista 4

---
+++ Ex3_Carro.java
@@ -0,0 +1,32 @@
+public class Ex3_Carro {
+    private String placa, piloto;
+
+    public Ex3_Carro(String placa, String piloto){
+        this.placa = placa;
+        this.piloto = piloto;
+    }
+
+    public void acelerar(){
+        System.out.println("Acelerando");
+    }
+
+    public void frear(){
+        System.out.println("Parando");
+    }
+
+    public String getPlaca(){
+        return placa;
+    }
+
+    public void setPlaca(String placa){
+        this.placa = placa;
+    }
+
+    public String getPiloto(){
+        return piloto;
+    }
+
+    public void setPiloto(String piloto){
+        this.piloto = piloto;
+    }    
+}
