45fe2d43c0e1cc78ccd895f5c91fc5337b04e4f5
Aluno <aluno@IPIRANGA.FATEC>
2022-04-07 16:57:21 -0300

    Commit fin

---
+++ Ex1_Carro.java
@@ -0,0 +1,30 @@
+public class Ex1_Carro{
+    private String placa, piloto;
+
+    public Ex1_Carro(){
+    }
+
+    public void acelerar(){
+        System.out.println("Acelerando");
+    }
+
+    public void frear(){
+        System.out.println("Parando");
+    }
+
+    public String getPlaca(){
+        return placa;
+    }
+
+    public void setPlaca(String placa){
+        this.placa = placa;
+    }
+
+    public String getPiloto(){
+        return piloto;
+    }
+
+    public void setPiloto(String piloto){
+        this.piloto = piloto;
+    }
+}
---
+++ Ex4_TesteCarro.java
@@ -0,0 +1,25 @@
+import static javax.swing.JOptionPane.showInputDialog;
+public class Ex4_TesteCarro {
+    public static void main(String[] args) {
+        String placa1, piloto1, placa2, piloto2;
+
+        placa1 = showInputDialog("Declare a placa");
+        piloto1 = showInputDialog("Declare o seu nome");
+
+        placa2 = showInputDialog("Declare a placa");
+        piloto2 = showInputDialog("Declare o seu nome");
+
+        Ex3_Carro carro1 = new Ex3_Carro(placa1, piloto1);
+        Ex3_Carro carro2 = new Ex3_Carro(placa2, piloto2);
+
+        carro1.getPlaca();
+        carro1.getPiloto();
+        carro1.acelerar();
+        carro1.frear();
+
+        carro2.getPlaca();
+        carro2.getPiloto();
+        carro2.acelerar();
+        carro2.frear();
+    }
+}
